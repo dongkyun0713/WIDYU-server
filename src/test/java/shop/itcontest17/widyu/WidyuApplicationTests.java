359aa538fa304f5ebf84d768c856051d9bc626d7
inhooo00 <chldlsgh0987@naver.com>
2025-08-16 01:19:50 +0900

    Setting: first commit

---
+++ src/test/java/shop/itcontest17/widyu/WidyuApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itcontest17.widyu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WidyuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
