ce4d9183e2549423b1e6b7975ca81744553d0220
dongkyunKim <kyun010713@gmail.com>
2025-08-20 14:36:13 +0900

    feature(#12): 멤버 레포지토리 작성

---
+++ src/main/java/com/widyu/member/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.widyu.member.repository;
+
+import com.widyu.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
