eed0a61f98081ee1d9d94cf8b751e736090eb9af
dongkyunKim <kyun010713@gmail.com>
2025-08-21 12:33:40 +0900

    docs(#12): 스웨거 문서 작성

--- src/main/java/com/widyu/auth/api/AuthController.java
+++ src/main/java/com/widyu/auth/api/AuthController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
-public class AuthController {
+public class AuthController implements AuthDocs {
     private final AuthService authService;
 
     @PostMapping("/sms/send")
