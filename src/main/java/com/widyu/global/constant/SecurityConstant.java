825912d98206d46959a5042a4e3bc74a8029fb40
dongkyunKim <kyun010713@gmail.com>
2025-08-21 12:31:45 +0900

    feature(#12): 전역 시큐리티 상수 추가

---
+++ src/main/java/com/widyu/global/constant/SecurityConstant.java
@@ -0,0 +1,10 @@
+package com.widyu.global.constant;
+
+public final class SecurityConstant {
+
+    // security
+    public static final String TOKEN_ROLE_NAME = "role";
+    public static final String TOKEN_PREFIX = "Bearer ";
+
+    private SecurityConstant() {}
+}
