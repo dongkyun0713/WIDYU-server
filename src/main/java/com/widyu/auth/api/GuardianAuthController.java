a014913bb10736cae6ad8df5e19bf35693607236
dongkyunKim <kyun010713@gmail.com>
2025-08-28 22:55:02 +0900

    refactor(#31): import문 수정

--- src/main/java/com/widyu/auth/api/GuardianAuthController.java
+++ src/main/java/com/widyu/auth/api/GuardianAuthController.java
@@ -13,7 +13,13 @@ import jakarta.validation.Valid;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
