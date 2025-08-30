54d1f59fc9b7673338c8e74d40457ba47e0ee698
dongkyunKim <kyun010713@gmail.com>
2025-08-30 22:29:10 +0900

    feature(#38): 액세스 노쿤 재발급 url 수정

--- src/main/java/com/widyu/auth/api/UnifiedAuthController.java
+++ src/main/java/com/widyu/auth/api/UnifiedAuthController.java
@@ -8,10 +8,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class UnifiedAuthController {
 
     private final AuthService authService;
